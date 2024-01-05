ba5287a28129048e962b0271c1b2947e406ea8a7
Herbert Marshall <herbert@herbmarshall.com>
2024-01-05 11:36:47 -0500

    UTIL-276 Add a bit of Javadoc to a SingletonRequireTest

--- src/test/java/com/herbmarshall/require/SingletonRequireTest.java
+++ src/test/java/com/herbmarshall/require/SingletonRequireTest.java
@@ -3,6 +3,12 @@ package com.herbmarshall.require;
 import com.herbmarshall.require.tester.RequireTestBuilder;
 import org.junit.jupiter.api.Test;
 
+/**
+ * {@link SingletonRequireTest} should be used with singleton objects and primitives.
+ * @param <T> The type of value
+ * @param <F> The {@link RequireFaultBuilder} type
+ * @param <R> The {@link Require} type
+ */
 abstract non-sealed class SingletonRequireTest<T, F extends RequireFaultBuilder<T, F>, R extends Require<T, F, R>>
 	extends BaseRequireTest<T, F, R> {
 
